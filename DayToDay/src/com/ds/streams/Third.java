20620b0c916975550b37d06dcf4fd8f6f1486eef
dsanap <dsanap391@gmail.com>
2025-01-10 12:23:32 +0530

    getting names of employee from list of employee

--- DayToDay/src/com/ds/streams/Third.java
+++ DayToDay/src/com/ds/streams/Third.java
@@ -7,13 +7,17 @@ import java.util.stream.Collectors;
 public class Third {
 	public static void main(String[] args) {
 		List<Employee> empList = Arrays.asList(
-				new Employee("Devansh",2),
-				new Employee("Aniket",20),
-				new Employee("Rushikesh",24),
-				new Employee("Shantanu",14),
-				new Employee("Praniti",15),
-				new Employee("Aradhya",8),
-				new Employee("Minal",19)
+				new Employee(12, "Aniket"),
+				new Employee(13, "Devansh"),
+				new Employee(12, "Aradhya"),
+				new Employee(14, "Minal"),
+				new Employee(13, "Rushikesh"),
+				new Employee(11, "Shantanu"),
+				new Employee(15, "Praniti"),
+				new Employee(14, "Soham"),
+				new Employee(16, "Vaishnavi"),
+				new Employee(17, "Prem"),
+				new Employee(11, "Krushna")
 			);
 		
 		List<String> nameList = empList.stream().map(name -> name.getName()).collect(Collectors.toList());
