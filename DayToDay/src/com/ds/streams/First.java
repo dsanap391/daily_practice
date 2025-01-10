992d5aa7df8e50040d1ee19f5e45e4bdd42bf46f
dsanap <dsanap391@gmail.com>
2025-01-10 11:51:25 +0530

    filter name with length greater than 3

--- DayToDay/src/com/ds/streams/First.java
+++ DayToDay/src/com/ds/streams/First.java
@@ -9,13 +9,17 @@ import java.util.stream.Collectors;
 public class First {
 	public static void main(String[] args) {
 			List<Employee> empList = Arrays.asList(
-					new Employee("Devansh",2),
-					new Employee("Aniket",20),
-					new Employee("Rushikesh",24),
-					new Employee("Shantanu",14),
-					new Employee("Praniti",15),
-					new Employee("Aradhya",8),
-					new Employee("Minal",19)
+					new Employee(12, "Aniket"),
+					new Employee(13, "Devansh"),
+					new Employee(12, "Aradhya"),
+					new Employee(14, "Minal"),
+					new Employee(13, "Rushikesh"),
+					new Employee(11, "Shantanu"),
+					new Employee(15, "Praniti"),
+					new Employee(14, "Soham"),
+					new Employee(16, "Vaishnavi"),
+					new Employee(17, "Prem"),
+					new Employee(11, "Krushna")
 				);
 				
 		List<Employee> nameList =	empList.stream().filter(name -> name.getName().length() > 5).collect(Collectors.toList());
